f2a63a52c56e7089c5577cd24ec7db0683aa27d9
Mahlke <964820@corp.trizetto.com>
2024-03-27 16:00:48 -0600

    fixed .env

--- app/wiki_fetcher.py
+++ app/wiki_fetcher.py
@@ -17,7 +17,8 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import Settings, load_index_from_storage, StorageContext
 from llama_index.agent.openai import OpenAIAgent
 from llama_index.core.node_parser import SentenceSplitter
-
+from dotenv import load_dotenv
+import os
 # Function Definitions
 
 def setup_environment(api_key):
@@ -207,8 +208,8 @@ def initialize_top_agent(obj_index):
 
 def main():
     # Setup environment and API key
-    api_key = "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"
-    setup_environment(api_key)
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
 
     # Define or load your wiki titles
     wiki_titles = [
