33839f954ea63f93206df5e61b9ea5815e52ab41
Maximillian Mahlke <themaxta@gmail.com>
2024-03-29 10:29:45 -0600

    added cors

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI, Request
 from main import fetch_response
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+ 
+origins = [
+    "http://localhost:7191",
+    "https://localhost:7191",
+    "https://localhost:7176"
+]
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 ## EXAMPLE OF USE:
