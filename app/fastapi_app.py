f076456b89d03c6bc6553234aead9fa1a8f2dbf7
Maximillian Mahlke <themaxta@gmail.com>
2024-03-29 10:17:05 -0600

    changes

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from main import fetch_response
 
 app = FastAPI()
@@ -7,10 +7,19 @@ app = FastAPI()
 ## EXAMPLE OF USE:
 
 #http://localhost:8000/query/?query=YourQueryHere
-#http://localhost:8000/query/?query=what is the difference between Cigna and Aetna?
+    #http://localhost:8000/query/?query=what&is&thedifference&between&Cigna&and&Aetna?
+
 
 @app.get("/query/")
-async def query_endpoint(query: str):
-    """Endpoint to fetch a response for a given query."""
+async def get_query(query: str):
+    """GET endpoint to fetch a response for a given query passed as a URL parameter."""
+    response = fetch_response(query)
+    return {"response": response}
+
+@app.post("/query/")
+async def post_query(request: Request):
+    """POST endpoint to fetch a response for a given query passed in the request body."""
+    data = await request.json()
+    query = data.get("InputText")
     response = fetch_response(query)
     return {"response": response}
